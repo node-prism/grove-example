2546846a67bd700e8144e79d50d0016ee5f65195
nvms <pyersjonathan@gmail.com>
2022-10-22 22:48:07 -0600

    Demonstrate `throttle` middleware usage.

--- src/app/http/_middleware.ts
+++ src/app/http/_middleware.ts
@@ -1,9 +1,12 @@
 import { Context } from "@prsm/server/http";
 import { log, LogLevel } from "@prsm/server/shared";
+import throttle from "@prsm/server/throttle";
 
 export default [
   async (c: Context) => {
     log({ level: LogLevel.INFO }, c.req.method, c.req.path, c.req.ip);
     c.next();
   },
+  async (c: Context) =>
+    throttle(c, { byIp: true, byPath: true, ipRate: 20, pathRate: 5 }),
 ];
